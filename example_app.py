945867c76518453db72e27d9f982d88fcc6cc1e7
Dave Kemp <dpkemp@missi.ncsc.mil>
2017-03-17 11:20:08 -0400

    Update example app to show message strings and APIs

--- example_app.py
+++ example_app.py
@@ -1,6 +1,7 @@
 import os
 import json
 from jaen.codec.codec import Codec
+from jaen.codec.codec_utils import flatten
 from jaen.codec.jaen import jaen_load
 
 # OpenC2 producer application:
@@ -11,10 +12,10 @@ command = {
         "domain_name": {
             "value": "cdn.badco.org"}}}
 
-schema = jaen_load(os.path.join("schema", "openc2.jaen"))     # Load and validate the OpenC2 schema
+schema = jaen_load(os.path.join("schema", "openc2.jaen"))   # Load and validate the OpenC2 schema
 codec = Codec(schema, verbose_rec=True, verbose_str=True)   # Create an OpenC2 encoder/decoder (JSON-Verbose encoding)
 message1 = codec.encode("OpenC2Command", command)           # Validate and encode the command
-print("Sent Message =", json.dumps(message1))
+print("Sent Message (JSON-v string)     =", json.dumps(message1))
 
 # OpenC2 consumer application:
 
@@ -22,4 +23,6 @@ received_msg = '[32,{"7":["cdn.badco.org"]}]'               # Received OpenC2 co
 message2 = json.loads(received_msg)
 codec.set_mode(verbose_rec=False, verbose_str=False)        # Tell codec to use JSON-minified encoding
 command2 = codec.decode("OpenC2Command", message2)          # Validate and decode the command
-print("Received Command =", command2)
+print("Received Message (JSON-m string) =", received_msg)
+print("Decoded Command (API structure)  =", command2)
+print("Decoded Command (API flat)       =", flatten(command2))

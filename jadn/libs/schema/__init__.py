62e9807c5755a8732b9cc26a5dbb553d63502888
Ian Cordova <Ian.Cordova@g2-inc.net>
2018-06-13 13:10:11 -0400

    minor changes to support cddl_jadn.py

--- jadn/libs/schema/__init__.py
+++ jadn/libs/schema/__init__.py
@@ -1,10 +1,7 @@
 from .cddl_jadn import cddl2jadn_dump, cddl2jadn_dumps
 from .proto_jadn import proto2jadn_dump, proto2jadn_dumps
 from .relax_jadn import relax2jadn_dump, relax2jadn_dumps
-from .cddl_jadn import cddl2jadn_dump, cddl2jadn_dumps
-=======
 from .thrift_jadn import thrift2jadn_dump,thrift2jadn_dumps
->>>>>>> 4960e39d1abb97cc9473ac4044b6035e4df742dc
 
 __all__ = [
     'cddl2jadn_dump',
@@ -13,11 +10,6 @@ __all__ = [
     'proto2jadn_dumps',
     'relax2jadn_dump',
     'relax2jadn_dumps',
-    'cddl2jadn_dump',
-    'cddl2jadn_dumps'
-=======
     'thrift2jadn_dump',
     'thrift2jadn_dumps'
->>>>>>> 4960e39d1abb97cc9473ac4044b6035e4df742dc
 ]
-git

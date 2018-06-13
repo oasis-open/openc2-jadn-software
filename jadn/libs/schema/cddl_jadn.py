a3501c5993a08858dfeb1b6eca4200a8cb4acde4
Ian Cordova <Ian.Cordova@g2-inc.net>
2018-06-13 13:09:39 -0400

    completed implementation w/o arpeggio

--- jadn/libs/schema/cddl_jadn.py
+++ jadn/libs/schema/cddl_jadn.py
@@ -20,7 +20,7 @@ class CDDLtoJADN(object):
             ['Choice', self._format_choice],
             ['Map', self._format_map],
             ['Enumerated', self._format_enumerated],
-            ['Array', self._format_tmp],
+            ['Array', self._format_tmp],  # tmp until we have a schema to go off of
             ['ArrayOf', self._format_array_of]
         ]
 
@@ -32,11 +32,16 @@ class CDDLtoJADN(object):
         """
 
         jadn = self.make_jadn()
-        print(Utils.jadnFormat(jadn, indent=2))
+        #(Utils.jadnFormat(jadn, indent=2))
         return Utils.jadnFormat(jadn, indent=2)
 
     def make_jadn(self):
-        is_meta = True
+        """
+        Converts cddl schema in to jadn format
+        :return: data in jadn readable format
+        :rtype: dict
+        """
+        section = "meta"
         meta = {}
         types = []
         jadn = {
@@ -46,22 +51,25 @@ class CDDLtoJADN(object):
 
         data = self._cddl.split(os.linesep)
         type_def = []
-        for line in data[:-1]:
-            #print(len(line))
+        for line in data[1:-1]:
             if line == '; types':
-                is_meta = False
+                section = 'types'
+                continue
+            elif line == '; Custom Defined Types':
+                section = 'custom'
                 continue
 
-            if is_meta:
+            if section == 'meta' and len(line) > 0:
                 meta.update(self.make_meta(line))
-            else:
+            elif section == 'types':
                 if len(line) > 0:
                     type_def.append(line)
                 else:
-                    #print("READING TYPE...")
-                    #print(type_def)
                     types.append(self.make_types(type_def))
                     type_def = []
+            elif section == 'custom' and len(line) > 0:
+                types.append(self.make_custom(line))
+
         return jadn
 
     def make_meta(self, meta_line):
@@ -73,13 +81,15 @@ class CDDLtoJADN(object):
         tmp = {}
 
         # removes leading '; ' from cddl
-        line = re.sub(r'^;\s', '', meta_line).split(' - ')
+        line = re.sub(r'^;\smeta:\s', '', meta_line)
+        entry = re.search(r'^([a-z]+)', line).group(1)
+        data = re.search(r'-\s(.*)', line).group(1)
 
         # convert to jadn readable format
         try:
-            tmp[line[0]] = json.loads(' - '.join(line[0:]))
+            tmp[entry] = json.loads(data)
         except Exception as e:
-            tmp[line[0]] = ' - '.join(line[0:])
+            tmp[entry] = data
 
         return tmp
 
@@ -94,7 +104,36 @@ class CDDLtoJADN(object):
             if item[0] in type_def[0]:
                 return item[1](type_def)
 
-        return self._format_tmp(type_def)
+        return self._format_array_of(type_def[0])
+
+    def make_custom(self, current_custom):
+        """
+        Creates type definitions from custom types for JADN Schema
+        - Assumes custom type is only a single line
+        :param current_custom:
+        :return:
+        """
+        custom_type = []
+
+        name_type = current_custom.split(' = ')
+        name = name_type[0][0:-1]
+        type_comment = name_type[1]
+        type = type_comment.split(';')[0][1:-1]
+        if type == 'bstr':
+            type = 'String'
+        comment = type_comment.split(';')[1][1:]
+
+        custom_type.append(name)
+        custom_type.append(type)
+        if 'TBD syntax' not in current_custom:
+            custom_type.append(["@" + name])
+        else:
+            custom_type.append([])
+        custom_type.append(comment)
+
+        return custom_type
+
+
 
     def _format_record(self, item):
         """
@@ -132,7 +171,7 @@ class CDDLtoJADN(object):
             # remove whitespace/unwanted characters
             pre_comment = pre_comment[2:]
             data_entry.append(re.search(r'^(.*):', pre_comment).group(1))
-            data_entry.append(re.search(r'type\": \"(.*)\",', line).group(1))
+            data_entry.append(re.search(r'type\":\s\"(.*?)\"', line).group(1))
 
             if is_optional:
                 data_entry.append(["[0"])
@@ -180,9 +219,10 @@ class CDDLtoJADN(object):
 
     def _format_choice(self, item):
         """
-
-        :param item:
-        :return:
+        Formats the type: Choice, under types, to the JADN schema
+        :param item: data to be converted
+        :return: data in JADN format
+        :rtype: list
         """
         choice = []
         choice.append(item[0].split(' ')[0].replace("_", "-"))
@@ -203,7 +243,7 @@ class CDDLtoJADN(object):
                 data_entry.append(int(field_num.group(1)))
 
             data_entry.append(line.split(":")[0][2:])
-            data_entry.append(re.search(r'type\": \"(.*)\",', line).group(1))
+            data_entry.append(re.search(r'type\": \"(.*)\"', line).group(1))
             data_entry.append([])
 
             comment = re.search(r'; (.*) #', line)
@@ -219,9 +259,10 @@ class CDDLtoJADN(object):
 
     def _format_map(self, item):
         """
-
-        :param item:
-        :return:
+        Formats the type: Map, under types, to the JADN schema
+        :param item: data to be converted
+        :return: data in JADN format
+        :rtype: list
         """
         map = []
         is_optional = False
@@ -267,8 +308,37 @@ class CDDLtoJADN(object):
         return map
 
     def _format_array_of(self, item):
+        """
+        Converts item of type ArrayOf to JADN format
+        - note: based off of one example in schema, ie. subject to change
+        :param item:
+        :return:
+        """
         arrayOf = []
+        arr_name = item.split('=')[0][:-1]
+        arrayOf.append(arr_name)
+        arrayOf.append('ArrayOf')
 
+        # put values inside outer_values as workaround for the way the jadn formatter seems to be working
+        outer_values = []
+        values = []
+
+        # parse down to the items within brackets
+        data_values = re.search(r'\[(.*)\]', item).group(1)
+
+        # parse out ex: 0*3 Query_Item
+        min = int(re.search(r'^([0-9]+)', data_values).group(1))
+        max = int(re.search(r'.([0-9]+)', data_values).group(1))
+        expr = re.search(r'[0-9]+(.)[0-9]+', data_values).group(1)
+        type = re.search(r'\s(.+)$', data_values).group(1)
+
+        values.append(expr+type)
+        values.append('[' + str(min))
+        values.append(']' + str(max))
+        values.append(item.split(';')[1][1:])
+
+        outer_values.append(values)
+        arrayOf.append(outer_values)
         return arrayOf
 
     def _format_tmp(self, item):

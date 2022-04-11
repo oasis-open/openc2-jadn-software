8b6c2d53b9d5505260d6e381a3287f7a44ba4471
David Kemp <19152940+davaya@users.noreply.github.com>
2022-04-11 11:35:53 -0400

    Clear for new version

--- jadn/libs/__init__.py
+++
@@ -1,6 +0,0 @@
-from .enums import OpenC2MessageFormats, OpenC2SchemaFormats
-
-__all__ = [
-    'OpenC2MessageFormats',
-    'OpenC2SchemaFormats'
-]
--- jadn/libs/message/proto/__init__.py
+++
@@ -1,6 +0,0 @@
-from .openc2_wd06_pb2 import OpenC2_Command, OpenC2_Response
-
-__all__ = [
-    'OpenC2_Command',
-    'OpenC2_Response'
-]

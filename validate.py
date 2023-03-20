ab92910f7c62b9e2429145a885e8635301487919
David Kemp <19152940+davaya@users.noreply.github.com>
2023-03-20 16:14:40 -0400

    update spdxv3 to current model

--- validate.py
+++ validate.py
@@ -6,13 +6,22 @@ import os
 SCHEMA_DIR = 'Schemas'
 DATA_DIR = 'Data'
 
+"""
+Validate a file against a JADN schema
+"""
 def validate(file: str = 'container.json', schema: str = 'container.jidl') -> None:
     filename, ext = os.path.splitext(file)
     with open(os.path.join(SCHEMA_DIR, schema), encoding='utf-8') as fp:
         sc = jadn.load_any(fp)
+    codec = jadn.codec.Codec(sc, verbose_rec=True, verbose_str=True)
+    item_type = sc['info']['exports'][0]
     with open(os.path.join(DATA_DIR, file), encoding='utf-8') as fp:
         data = json.load(fp)
-    print(len(sc), len(data))
+    print(f'{item_type}: {len(data)}')
+    try:
+        codec.decode(item_type, data)
+    except ValueError as e:
+        print(f' Error: {e}')
 
 if __name__ == '__main__':
     try:

a35992d950847ea924de2809b045abcb3be7bb05
David Kemp <19152940+davaya@users.noreply.github.com>
2024-06-18 10:50:05 -0400

    various test examples

--- resolve-references.py
+++ resolve-references.py
@@ -12,14 +12,14 @@ SCHEMA_DIR = 'Schemas'
 OUTPUT_DIR = 'Out'
 
 
-def resolve(schema: str = 'device-pac-slpf-base.jadn', reference_dir: str = SCHEMA_DIR, output_dir: str = OUTPUT_DIR) -> None:
+def resolve(schema: str, reference_dir: str = SCHEMA_DIR, output_dir: str = OUTPUT_DIR, merge_ns: str = '') -> None:
     print(f'Installed JADN version: {jadn.__version__}\n')
-    print(f'{reference_dir}/{schema} -> {output_dir}')
+    print(f'{reference_dir}/{schema} -> {output_dir}: merge={merge_ns}')
     os.makedirs(output_dir, exist_ok=True)
     filename, ext = os.path.splitext(schema)
     with open(os.path.join(reference_dir, schema), encoding='utf-8') as fp:
         sc = jadn.load_any(fp)                 # Load base package
-    sc2 = jadn.transform.resolve_imports(sc, reference_dir, ('ls',))        # Resolve referenced definitions
+    sc2 = jadn.transform.resolve_imports(sc, reference_dir, (merge_ns,))        # Resolve referenced definitions
     jadn.dump(sc2, os.path.join(output_dir, filename + '-resolved.jadn'))   # Save resolved base package
     print(f'{schema}:\n' + '\n'.join([f'{k:>14}: {v}' for k, v in jadn.analyze(jadn.check(sc2)).items()]))
 
